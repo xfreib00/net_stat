4c4f3d26fe2995be38abdefc6f64ea80a2f53d4d
xfreib00 <xfreib00@stud.fit.vutbr.cz>
2019-06-08 23:03:17 +0200

    Updated class Count_test and added KeyboardInterrupt handler

--- py_stats.py
+++ py_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import pyspeedtest
+from statistics import mean
 
 #class for measuring internet dowload,upload and ping
 class SP_Test: 
@@ -16,11 +17,41 @@ class SP_Test:
         self.test_list.append(self.st.upload())
         self.test_list.append(self.st.ping())    
     
-    #removing all items from list
-    def clear_list(self):
-        self.test_list.clear()
 
 #class for making stats from multiple tests
 class Count_test(SP_Test):
+
+    #creating 3 objects of SP_Test class and list for results
+    def __init__(self):
+        self.t1 = SP_Test()
+        self.t2 = SP_Test()
+        self.t3 = SP_Test()
+        self.result_list = []
+
+    #counting the average down,up and ping from 3 tests
+    def speed_avg(self):
+        self.t1.get_stats()
+        self.t2.get_stats()
+        self.t3.get_stats()
+        self.result_list = [mean(k) for k in zip(self.t1.test_list,self.t2.test_list,self.t3.test_list)]
+    
+    #printing result of average speeds
+    #strictly debugging function
+    def print_results(self):
+        print(*self.result_list)
+
+#creating object of Count_test class and running speed tests
+try:
+    X = Count_test()
+    X.speed_avg()
+    X.print_results()   
+
+#in case of Keyboard interrupt exit program immediately
+except KeyboardInterrupt:
+    exit(0)
+
+
+
+
 
 
