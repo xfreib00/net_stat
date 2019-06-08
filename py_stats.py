2420eab87a97a7672082f3045af60f2e93fb2d8e
Adam Freiberg <xfreib00@stud.fit.vutbr.cz>
2019-06-08 20:11:03 +0200

    Changed behaviour of class SP_Test and added new class

--- py_stats.py
+++ py_stats.py
@@ -1,22 +1,26 @@
+#!/usr/bin/env python3
 import sys
 import pyspeedtest
 
-class SP_Test:
-    
+#class for measuring internet dowload,upload and ping
+class SP_Test: 
 
+    #initialization of list and speedtest class
     def __init__(self):
-        self.test_dict = {}
+        self.test_list = []
         self.st = pyspeedtest.SpeedTest()
 
+    #measuring internet speeds and appeding results to the list
     def get_stats(self):
-        self.up = self.st.upload()
-        self.down = self.st.download()
-        self.ping = self.st.ping()
-
+        self.test_list.append(self.st.download())
+        self.test_list.append(self.st.upload())
+        self.test_list.append(self.st.ping())    
     
+    #removing all items from list
+    def clear_list(self):
+        self.test_list.clear()
 
-
-x = SP_Test()
-x.get_stats()
+#class for making stats from multiple tests
+class Count_test(SP_Test):
 
 
